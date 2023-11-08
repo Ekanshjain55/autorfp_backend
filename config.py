8eb051e3af0ddee23cc3e840bd1f86726cd9d4b5
ekanshjain55 <ekansh.jain@fiftyfivetech.io>
2023-11-08 12:29:03 +0530

    updated openai api_key

--- config.py
+++ config.py
@@ -1,2 +1,4 @@
-# OpenAI API key
-API_KEY = "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"
+import os
+
+#  OpenAI API key
+API_KEY = os.getenv("OPENAI_API_KEY", "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")
