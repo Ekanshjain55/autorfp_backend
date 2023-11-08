4ee635602b01bfdcf74926b1cef3820bb6fe7352
ekanshjain55 <ekansh.jain@fiftyfivetech.io>
2023-11-08 12:19:37 +0530

    xyz

--- api/openai_wrapper.py
+++ api/openai_wrapper.py
@@ -6,16 +6,14 @@ openai.api_key = API_KEY
 
 def get_chatgpt_response(prompt, model="gpt-3.5-turbo-16k", temperature=0.6, max_tokens=9000, n=1):
     messages = [{"role": "system", "content": "You are a Senior Software architect..."}, {"role": "user", "content": prompt}]
-    try:
-        response = openai.ChatCompletion.create(
-            model=model,
-            messages=messages,
-            temperature=temperature,
-            max_tokens=max_tokens,
-            n=n,
-            stop=None
-        )
-        generated_response = response['choices'][0]['message']['content'].strip()
-        return generated_response if generated_response else ''
-    except openai.OpenAIError as e:
-        raise Exception("Error occurred while retrieving response from OpenAI API.")
+
+    response = openai.ChatCompletion.create(
+        model=model,
+        messages=messages,
+        temperature=temperature,
+        max_tokens=max_tokens,
+        n=n,
+        stop=None
+    )
+    generated_response = response['choices'][0]['message']['content'].strip()
+    return generated_response if generated_response else ''
