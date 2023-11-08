77e20f971d04de4bbb182d8533e55adfd1729654
ekanshjain55 <ekansh.jain@fiftyfivetech.io>
2023-11-08 12:16:38 +0530

    Downgrade openai

--- api/openai_wrapper.py
+++ api/openai_wrapper.py
@@ -2,20 +2,20 @@ import openai
 from config import API_KEY
 
 openai.api_key = API_KEY
-client = openai.OpenAI(
-    api_key=API_KEY
-)
+
 
 def get_chatgpt_response(prompt, model="gpt-3.5-turbo-16k", temperature=0.6, max_tokens=9000, n=1):
     messages = [{"role": "system", "content": "You are a Senior Software architect..."}, {"role": "user", "content": prompt}]
-
-    response = client.chat.completions.create(
-        model=model,
-        messages=messages,
-        temperature=temperature,
-        max_tokens=max_tokens,
-        n=n,
-        stop=None
-    )
-    generated_response = response.choices[0].message.content.strip()
-    return generated_response if generated_response else ''
+    try:
+        response = openai.ChatCompletion.create(
+            model=model,
+            messages=messages,
+            temperature=temperature,
+            max_tokens=max_tokens,
+            n=n,
+            stop=None
+        )
+        generated_response = response['choices'][0]['message']['content'].strip()
+        return generated_response if generated_response else ''
+    except openai.OpenAIError as e:
+        raise Exception("Error occurred while retrieving response from OpenAI API.")
